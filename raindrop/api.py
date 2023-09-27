c5904e6ef151f873cc615295a98a8acc679b859a
Doreen2002 <doreenmwapekatebe8@gmail.com>
2023-09-27 05:56:39 +0200

    nepali date changes, and naming series on invoice

---
+++ raindrop/api.py
@@ -0,0 +1,16 @@
+import frappe
+from datetime import datetime
+import datetime as real_Date
+import nepali_datetime
+
+
+
+@frappe.whitelist()
+def get_nepali_date(date):
+    date_str = date
+    date_format = '%Y-%m-%d'
+    year = datetime.strptime(date_str, date_format).year
+    month = datetime.strptime(date_str, date_format).month
+    day = datetime.strptime(date_str, date_format).day
+    return str(nepali_datetime.date.from_datetime_date(real_Date.date(year, month, day)))
+    
