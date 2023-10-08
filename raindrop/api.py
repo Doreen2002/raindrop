5ad7c68569ef152c75b78a3b2fa560333a7d71c5
Doreen2002 <doreenmwapekatebe8@gmail.com>
2023-10-08 06:53:22 +0200

    modifed charter of accounts file upload function

--- raindrop/api.py
+++ raindrop/api.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import datetime as real_Date
 import nepali_datetime
 import pandas as pd
+import requests
 
 
 @frappe.whitelist()
@@ -17,40 +18,46 @@ def get_nepali_date(date):
 import csv
 @frappe.whitelist()
 def create_charter_of_accounts():
+    url = "https://hpl.raindropinc.com/files/HPL_COA_FINAL_NPR (3) - Sheet1 (1).csv" 
+    response = requests.get(url)
+    content = response.content.decode('utf-8')
+    reader = csv.reader(content.splitlines(), delimiter=',')
+    for row in reader:
+        try:
+            doc = frappe.new_doc('Account')
+            doc.account_type = row[1]
+            doc.root_type = row[2]
+            doc.report_type = row[3]
+            doc.internal_id = row[4]
+            doc.account_number = row[5]
+            doc.account_name = row[6]
+            doc.display_name = row[7]
+            doc.description = row[9]
+            doc.sun_account_code = row[10]
+            doc.is_disabled = row[11]
+            doc.attention = row[12]
+            doc.bank_account = row[13]
+            doc.equivalent_npr_tds_account = row[14]
+            doc.facsimile = row[15]
+            doc.payment_instruction = row[16]
+            doc.summary = row[17]
+            doc.localized_display_name = row[18]
+            doc.localized_name = row[19]
+            doc.tds_npr_account = row[20]
+            doc.tds_usd_account = row[21]
+            doc.salutation = row[22]
+            doc.subsidiary = row[23]
+            doc.parent_account = row[0]
+            doc.is_group = 0
+            doc.insert(ignore_mandatory=True, ignore_links=True)
+            frappe.db.commit()
+        except Exception as e:
+            print(f'{e}')
 
-    with open('https://hpl.raindropinc.com/files/HPL_COA_FINAL_NPR (3) - Sheet1 (1).csv', 'r', newline='') as csvfile:
-        csv_reader = csv.reader(csvfile)
-        for row in csv_reader:
-            try:
-                doc = frappe.new_doc('Account')
-                doc.account_type = row[1]
-                doc.root_type = row[2]
-                doc.report_type = row[3]
-                doc.internal_id = row[4]
-                doc.account_number = row[5]
-                doc.account_name = row[6]
-                doc.display_name = row[7]
-                doc.description = row[9]
-                doc.sun_account_code = row[10]
-                doc.is_disabled = row[11]
-                doc.attention = row[12]
-                doc.bank_account = row[13]
-                doc.equivalent_npr_tds_account = row[14]
-                doc.facsimile = row[15]
-                doc.payment_instruction = row[16]
-                doc.summary = row[17]
-                doc.localized_display_name = row[18]
-                doc.localized_name = row[19]
-                doc.tds_npr_account = row[20]
-                doc.tds_usd_account = row[21]
-                doc.salutation = row[22]
-                doc.subsidiary = row[23]
-                doc.parent_account = row[0]
-                doc.is_group = 0
-                doc.insert(ignore_mandatory=True, ignore_links=True)
-                frappe.db.commit()
-            except Exception as e:
-                print(f'{e}')
+
+  
+               
+            
             
           
 @frappe.whitelist()
