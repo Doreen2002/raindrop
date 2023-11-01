a5bb433de81ad0a9ed42ba702857ea6ca86a5ebf
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2023-11-01 15:14:09 +0545

    Update api.py

--- raindrop/api.py
+++ raindrop/api.py
@@ -755,7 +755,7 @@ def create_customer():
         doc.tax_number = row[37].strip()
         doc.credit_limit = row[38].strip()
         doc.customer_name = row[2].strip()
-        doc.doc.customer_group = ""
+        doc.customer_group = ""
         doc.territory = "Nepal"  
         doc.insert()
         frappe.db.commit()
