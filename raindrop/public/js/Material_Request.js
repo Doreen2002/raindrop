781042ff7fa8d9736a16844792db98b11aa37aff
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2023-12-12 12:16:34 +0545

    Added Material Request js file

---
+++ raindrop/public/js/Material_Request.js
@@ -0,0 +1,39 @@
+frappe.ui.form.on("Material Request", {
+
+
+    refresh(frm)
+    {
+        frm.add_custom_button(__("Create Purchase Order"), function() {
+            frappe.model.with_doctype('Purchase Order', function() {
+                var mr = frappe.model.get_new_doc('Purchase Order');
+                var items = frm.get_field('items').grid.get_selected_children();
+                if(!items.length) {
+                    items = frm.doc.items;
+                }
+
+                mr.work_order = frm.doc.work_order;
+                items.forEach(function(item) {
+                    var mr_item = frappe.model.add_child(mr, 'items');
+                    mr_item.item_code = item.item_code;
+                    mr_item.item_name = item.item_name;
+                    mr_item.uom = item.uom;
+                    mr_item.stock_uom = item.stock_uom;
+                    mr_item.conversion_factor = item.conversion_factor;
+                    mr_item.item_group = item.item_group;
+                    mr_item.description = item.description;
+                    mr_item.image = item.image;
+                    mr_item.qty = item.qty;
+                    mr_item.warehouse = item.s_warehouse;
+                    mr_item.schedule_date = frappe.datetime.nowdate();
+                });
+                frappe.set_route('Form', 'Purchase Order', mr.name);
+               
+            });
+
+            
+               
+        });
+    }
+
+
+})
