8e91a14113ff56042889fd4986276fe8b428954e
Doreen2002 <doreenmwapekatebe8@gmail.com>
2023-12-13 18:49:01 +0200

    modified

--- raindrop/custom_code/purchase_receipt.py
+++ raindrop/custom_code/purchase_receipt.py
@@ -4,7 +4,7 @@ import frappe
 
 def on_update(doc, method):
     po = doc.items[0].purchase_order
-    material = frappe.db.get_value("Purchase Order", {"parent":po}, 'custom_email_initiator')
+    material = frappe.db.get_value("Purchase Order", po, 'custom_email_initiator')
     material_req = material
     doc.custom_email__initiator = material_req
     
