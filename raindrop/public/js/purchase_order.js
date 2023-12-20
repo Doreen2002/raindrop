ef3093475122eec843fa6cb1b2ec7748b8546c7f
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2023-12-20 23:07:36 +0200

    Create purchase_order.js

---
+++ raindrop/public/js/purchase_order.js
@@ -0,0 +1,48 @@
+frappe.ui.form.on("Purchase Order", {
+
+
+    refresh(frm)
+    {
+        frappe.call({
+            method: 'raindrop.api.get_nepali_date',
+            args: {
+                date: frm.doc.posting_date
+            },
+            freeze: true,
+            callback: (r) => {
+                frm.doc.custom_nepali_date = r.message
+                frm.refresh_fields()
+            },
+            error: (r) => {
+                console.log(r)
+            }
+        })
+
+    },
+      
+    posting_date(frm)
+    {
+        frappe.call({
+            method: 'raindrop.api.get_nepali_date',
+            args: {
+                date: frm.doc.posting_date
+            },
+            freeze: true,
+            callback: (r) => {
+                frm.doc.custom_nepali_date = r.message
+                frm.refresh_fields()
+            },
+            error: (r) => {
+                console.log(r)
+            }
+        })
+    },
+    
+})
+
+
+
+function convertToNepaliDate(gregorianDate) {
+    const nepaliDate = NepaliDateConverter.convertToNepali(gregorianDate);
+    return `${nepaliDate.year}-${nepaliDate.month}-${nepaliDate.day}`;
+  }
