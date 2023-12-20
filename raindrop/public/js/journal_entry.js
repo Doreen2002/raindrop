e92855fcbfa344a48619ba5e2169a0252dc7dcdf
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2023-12-20 22:51:34 +0200

    Create journal_entry.js

---
+++ raindrop/public/js/journal_entry.js
@@ -0,0 +1,34 @@
+frappe.ui.form.on("Journal Entry", {
+
+
+    refresh(frm)
+    {
+
+    },
+      
+    posting_date(frm)
+    {
+        frappe.call({
+            method: 'raindrop.api.get_nepali_date',
+            args: {
+                date: frm.doc.posting_date
+            },
+            freeze: true,
+            callback: (r) => {
+                frm.doc.custom_nepali_date = r.message
+                frm.refresh()
+            },
+            error: (r) => {
+                console.log(r)
+            }
+        })
+    },
+    
+})
+
+
+
+function convertToNepaliDate(gregorianDate) {
+    const nepaliDate = NepaliDateConverter.convertToNepali(gregorianDate);
+    return `${nepaliDate.year}-${nepaliDate.month}-${nepaliDate.day}`;
+  }
