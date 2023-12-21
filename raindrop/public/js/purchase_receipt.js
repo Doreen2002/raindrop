d56df4501260fa10ffeeaef95405a52af62a9253
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2023-12-21 06:50:42 +0200

    Update purchase_receipt.js

--- raindrop/public/js/purchase_receipt.js
+++ raindrop/public/js/purchase_receipt.js
@@ -1,18 +1,16 @@
 frappe.ui.form.on("Purchase Receipt", {
 
-
+onload_post_render: function(frm){
+        var bt = ['Make Stock Entry', 'Purchase Return',  'Retention Stock Entry', 'Subscription']
+        bt.forEach(function(bt){
+            frm.page.remove_inner_button(bt, 'Create')
+            });
+        
+        }
+    },
     refresh(frm)
     {
         $("button:contains('Get Items From')").hide();
-	    setTimeout(() =>{
-        frm.remove_custom_button( 'Make Stock Entry', 'Create');
-        }, 500);
-     //    setTimeout(() => {
-	    //     frm.page.actions.find('[data-label="Make%20Stock%20Entry"]').parent().parent().remove();
-     //        frm.page.actions.find('[data-label="Purchase%20Return"]').parent().parent().remove();
-     //        frm.page.actions.find('[data-label="Retention%20Stock%20Entry"]').parent().parent().remove();
-     //        frm.page.actions.find('[data-label="Subscription"]').parent().parent().remove();
-	    // }, 500);
 
         frappe.call({
             method: 'raindrop.api.get_nepali_date',
