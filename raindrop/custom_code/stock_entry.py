8ebf8e92a8b8925839582f1e231bedd11d4e3e1b
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2023-12-28 11:39:36 +0200

    Update stock_entry.py

--- raindrop/custom_code/stock_entry.py
+++ raindrop/custom_code/stock_entry.py
@@ -2,3 +2,13 @@ from erpnext.stock.doctype.stock_entry.stock_entry import StockEntry
 def on_save(doc, method):
     if doc.stock_entry_type == "Material Transfer":
         StockEntry.on_submit(doc)
+def on_update(doc, method):
+    purchase_approver = frappe.db.get_value("Employee", {"user_id":doc.owner}, "custom_purchase_approver_id")
+    if purchase_approver == '' or purchase_approver == None and "Administrator"  in frappe.get_roles():
+        frappe.throw("Please ask Administrator to set Purchase Approver For you")
+
+@frappe.whitelist()
+def add_approver(owner):
+    purchase_approver = frappe.db.get_value("Employee", {"user_id":owner}, "custom_purchase_approver_id")
+    if purchase_approver != '' or purchase_approver != None:
+        return purchase_approver
