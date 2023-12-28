ced5c8ae03789af2c02dc00554e9a5bf0a9a2b39
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2023-12-28 07:05:37 +0200

    Update internal_transfer.py
    updated validation for purchase approver

--- raindrop/custom_code/internal_transfer.py
+++ raindrop/custom_code/internal_transfer.py
@@ -5,7 +5,7 @@ def on_update(doc, method):
     purchase_approver = frappe.db.get_value("Employee", {"user_id":doc.owner}, "custom_purchase_approver_id")
     if purchase_approver == '' or purchase_approver == None:
         frappe.throw("Please ask Administrator to set Purchase Approver For you")
-    if purchase_approver == '' or purchase_approver == None:
+    if purchase_approver != '' or purchase_approver != None:
         doc.custom_purchase_approver__id = purchase_approver
 
 
