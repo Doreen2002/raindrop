f8178f075164b4266f327d12c1d2d1847fa77adc
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2023-12-28 11:44:36 +0200

    Update purchase_invoice.py

--- raindrop/custom_code/purchase_invoice.py
+++ raindrop/custom_code/purchase_invoice.py
@@ -37,5 +37,16 @@ def on_submit(doc, method):
     journal.insert()
     frappe.db.commit()
     doc.custom_journal_entry= journal.name
+
+def on_update(doc, method):
+    purchase_approver = frappe.db.get_value("Employee", {"user_id":doc.owner}, "custom_purchase_approver_id")
+    if purchase_approver == '' or purchase_approver == None and "Administrator"  in frappe.get_roles():
+        frappe.throw("Please ask Administrator to set Purchase Approver For you")
+
+@frappe.whitelist()
+def add_approver(owner):
+    purchase_approver = frappe.db.get_value("Employee", {"user_id":owner}, "custom_purchase_approver_id")
+    if purchase_approver != '' or purchase_approver != None:
+        return purchase_approver
               
             
