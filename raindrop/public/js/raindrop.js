fa1c7d79211a82991f7be232c2ea8d00f22b93a3
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2023-12-28 05:43:57 +0200

    Update raindrop.js

--- raindrop/public/js/raindrop.js
+++ raindrop/public/js/raindrop.js
@@ -1,9 +1,13 @@
 setInterval(
     ()=>
     {
+        if (!frappe.user.has_role("Administrator"))
+        {
+            
           $('button[data-label="Create%20Workspace"]').hide()
         $('button[data-label="Edit"]').hide()
       console.log("hai")
+        }
           
     },
     100
