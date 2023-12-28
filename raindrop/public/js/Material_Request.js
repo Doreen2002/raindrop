0c50b85b0751b23055aeacd914463dfa75bc3194
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2023-12-28 08:23:25 +0200

    Update Material_Request.js
    Hiding purchase approver id

--- raindrop/public/js/Material_Request.js
+++ raindrop/public/js/Material_Request.js
@@ -37,6 +37,7 @@ frappe.ui.form.on("Material Request", {
         $('button:contains("Create")').hide();
          $('button:contains("Get Items From")').hide();
         $('div[data-fieldname="custom_email_initiator_"]').hide();
+        $('div[data-fieldname="custom_purchase_approver__id"]').hide();
         if (cur_frm.doc.material_request_type == "Purchase" && frappe.user.has_role('HPL Inventory') && cur_frm.doc.workflow_state == "Approved")
         {
             frm.add_custom_button(__("Create Purchase Order"), function() {
