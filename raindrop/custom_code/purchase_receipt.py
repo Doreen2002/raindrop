93597d4395c87c198c4035283098f78c3bc892a8
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2023-12-28 11:42:28 +0200

    Update purchase_receipt.py

--- raindrop/custom_code/purchase_receipt.py
+++ raindrop/custom_code/purchase_receipt.py
@@ -7,7 +7,15 @@ def on_update(doc, method):
     material = frappe.db.get_value("Purchase Order", po, 'custom_email_initiator')
     material_req = material
     doc.custom_email__initiator = material_req
-    
+    purchase_approver = frappe.db.get_value("Employee", {"user_id":doc.owner}, "custom_purchase_approver_id")
+    if purchase_approver == '' or purchase_approver == None and "Administrator"  in frappe.get_roles():
+        frappe.throw("Please ask Administrator to set Purchase Approver For you")
+
+@frappe.whitelist()
+def add_approver(owner):
+    purchase_approver = frappe.db.get_value("Employee", {"user_id":owner}, "custom_purchase_approver_id")
+    if purchase_approver != '' or purchase_approver != None:
+        return purchase_approver
        
     
 
