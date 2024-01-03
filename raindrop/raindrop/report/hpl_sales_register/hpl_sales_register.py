7de8a61bfcf204405ca4a62c9527e9ab71c1198d
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-01-03 13:01:04 +0200

    Update hpl_sales_register.py

--- raindrop/raindrop/report/hpl_sales_register/hpl_sales_register.py
+++ raindrop/raindrop/report/hpl_sales_register/hpl_sales_register.py
@@ -153,9 +153,12 @@ def execute(filters=None):
 	sales_invoice = frappe.db.get_list("Sales Invoice", filters={"docstatus":1}, fields=['*'])
 	for sale in sales_invoice:
 		items = frappe.db.get_all("Sales Invoice Item", filters={"parent":sale.name}, fields=['*'])
+		total = 0
 		for item in items:
-			data.append([sale.posting_date, sale.custom_document_number, sale.customer, '', '', '', item.description, item.uom, item.qty, item.rate, item.amount, '', '', '', '', '', '', '', '', ''])
-	
+			total_amount += item.amount
+			total + = item.amount
+			data.append([sale.posting_date, sale.custom_document_number, sale.customer, sale.custom_billing_address, '', '', '', item.description, item.uom, item.qty, item.rate, item.amount, '', '', '', '', '', '', '', total_amount])
+		data.append(['', 'Total Invoice', '', '', '', '', '', '', total, '', '', '', '', '', '', '', '', '', '', total_amount])	
 	return columns, data
 
 
