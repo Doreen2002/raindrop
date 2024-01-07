c6797c6ffb28edd67277420be19883a24935a8fa
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-01-07 21:26:48 +0200

    Create expense_claim.js

---
+++ raindrop/public/js/expense_claim.js
@@ -0,0 +1,63 @@
+frappe.ui.form.on("Expense Claim", {
+onload_post_render: function(frm){
+	
+if (frm.doc.workflow_state == "Pending" && frm.doc.custom_purchase_approver__id != frappe.session.logged_in_user && !frappe.user.has_role("Administrator"))
+	 {
+		$('.actions-btn-group').hide()
+	}
+	if ( frm.doc.workflow_state == "Draft" )
+	{
+		$('.actions-btn-group').show()
+	}
+  
+	cur_frm.set_df_property('approval_status', 'hidden', 1);
+  cur_frm.set_df_property('expense_approver', 'hidden', 1);
+  cur_frm.refresh_fields();
+	
+
+},
+
+    before_save(frm)
+        {
+            frappe.call({
+            method: 'raindrop.custom_code.internal_transfer.add_approver',
+            args: {
+                owner: frm.doc.owner
+            },
+            freeze: true,
+            callback: (r) => {
+                frm.doc.expense_approver = r.message;
+                frm.refresh_fields();
+            },
+            error: (r) => {
+                console.log(r)
+            }
+            
+        })
+        },
+
+    refresh(frm)
+    {
+        frappe.call({
+            method: 'raindrop.api.get_nepali_date',
+            args: {
+                date: frm.doc.posting_date
+            },
+            freeze: true,
+            callback: (r) => {
+                frm.doc.custom_nepali_date = r.message
+                frm.refresh_fields()
+            },
+            error: (r) => {
+                console.log(r)
+            }
+        })
+
+    },
+      
+
+    
+})
+
+
+
