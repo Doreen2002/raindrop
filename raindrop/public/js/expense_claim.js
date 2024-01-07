a5f89d574adee1247553d5795d3d6839015ee68c
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-01-07 21:29:06 +0200

    Update expense_claim.js

--- raindrop/public/js/expense_claim.js
+++ raindrop/public/js/expense_claim.js
@@ -20,7 +20,7 @@ if (frm.doc.workflow_state == "Pending" && frm.doc.custom_purchase_approver__id 
     before_save(frm)
         {
             frappe.call({
-            method: 'raindrop.custom_code.internal_transfer.add_approver',
+            method: 'raindrop.custom_code.expense_claim.add_approver',
             args: {
                 owner: frm.doc.owner
             },
