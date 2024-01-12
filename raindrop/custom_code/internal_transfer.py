62354b7407ef24d99210a52b963422d3bd535f5d
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-01-12 13:38:18 +0200

    Update internal_transfer.py

--- raindrop/custom_code/internal_transfer.py
+++ raindrop/custom_code/internal_transfer.py
@@ -2,13 +2,19 @@ import frappe
 from frappe.utils import today
 
 def on_update(doc, method):
-    purchase_approver = frappe.db.get_value("Employee", {"user_id":doc.owner}, "custom_purchase_approver_id")
+    #get logged emloyee ID
+    employee = frappe.db.get_value("Employee", {"user_id":doc.owner}, "name")
+    #get logged supervisor at particular cost center
+    purchase_approver = frappe.db.get_value("Employee Cost Center Manager", {"parent":employee, "cost_center":cost_center}, "supervisor")
     if purchase_approver == '' or purchase_approver == None and "Administrator" not in frappe.get_roles():
         frappe.throw("Please ask Administrator to set Purchase Approver For you")
 
 @frappe.whitelist()
-def add_approver(owner):
-    purchase_approver = frappe.db.get_value("Employee", {"user_id":owner}, "custom_purchase_approver_id")
+def add_approver(owner, custom_cost_center):
+    #get logged emloyee ID
+    employee = frappe.db.get_value("Employee", {"user_id":doc.owner}, "name")
+    #get logged supervisor at particular cost center
+    purchase_approver = frappe.db.get_value("Employee Cost Center Manager", {"parent":employee, "cost_center":cost_center}, "supervisor")
     if purchase_approver != '' or purchase_approver != None:
         return purchase_approver
 
