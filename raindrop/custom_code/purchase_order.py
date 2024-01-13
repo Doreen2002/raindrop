5cb946bc0f914785a2e532bac10d928e0080db6b
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-01-13 08:59:36 +0200

    Update purchase_order.py

--- raindrop/custom_code/purchase_order.py
+++ raindrop/custom_code/purchase_order.py
@@ -8,7 +8,10 @@ def before_insert(doc, method):
 
     
 def on_update(doc, method):
-    purchase_approver = frappe.db.get_value("Employee", {"user_id":doc.owner}, "custom_purchase_approver_id")
+    #get logged emloyee ID
+    employee = frappe.db.get_value("Employee", {"user_id":doc.owner}, "name")
+    #get logged supervisor at particular cost center
+    purchase_approver = frappe.db.get_value("Employee Cost Center Manager", {"parent":employee, "cost_center":doc.custom_cost_center}, "supervisor")
     if purchase_approver == '' or purchase_approver == None and "Administrator" not in frappe.get_roles():
         frappe.throw("Please ask Administrator to set Purchase Approver For you")
     if doc.workflow_state == "Approved":
@@ -23,11 +26,20 @@ def on_update(doc, method):
                
     
 
-    
-
 @frappe.whitelist()
-def add_approver(owner, cost_center):
+def add_approver(owner, custom_cost_center):
+    #get logged emloyee ID
     employee = frappe.db.get_value("Employee", {"user_id":owner}, "name")
-    purchase_approver = frappe.db.get_value("Employee Cost Center Manager", {"parent":employee, "cost_center":cost_center}, "supervisor")
+    #get logged supervisor at particular cost center
+    purchase_approver = frappe.db.get_value("Employee Cost Center Manager", {"parent":employee, "cost_center":custom_cost_center}, "supervisor")
     if purchase_approver != '' or purchase_approver != None:
-        return purchase_approver
+        return purchase_approver    
+
+
+
+
+
+
+
+
+
