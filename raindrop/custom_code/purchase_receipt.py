0b7112278e98792d7eb94ec6dbdb63f3130560f6
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-01-15 05:18:14 +0200

    Update purchase_receipt.py

--- raindrop/custom_code/purchase_receipt.py
+++ raindrop/custom_code/purchase_receipt.py
@@ -18,6 +18,17 @@ def add_approver(owner, custom_cost_center):
     purchase_approver = frappe.db.get_value("Employee Cost Center Manager", {"parent":employee, "cost_center":custom_cost_center}, "supervisor")
     if purchase_approver != '' or purchase_approver != None:
         return purchase_approver
+
+@frappe.whitelist()
+def get_approver(owner):
+    approver_list = []
+    employee = frappe.db.get_value("Employee", {"user_id":owner}, "name")
+    approvers = frappe.db.get_all("Employee Cost Center Manager", filters={"parent":employee}, fields=['*'])
+    if approvers == []:
+        frappe.throw("Please ask Administrator to set Purchase Approver For you")
+    for appr in approvers:
+        approver_list.append(appr.cost_center)
+    return approver_list
        
     
 
