17242e9b874f1749782ae02cc33bccd610a1397c
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-01-21 04:50:33 +0200

    Update stock_entry.py

--- raindrop/custom_code/stock_entry.py
+++ raindrop/custom_code/stock_entry.py
@@ -2,7 +2,7 @@ from erpnext.stock.doctype.stock_entry.stock_entry import StockEntry
 import frappe
 def on_save(doc, method):
     if doc.stock_entry_type == "Material Issue":
-        StockEntry.on_submit.set_actual_qty(doc)
+        StockEntry.validate(doc)
 def on_update(doc, method):
     #get logged emloyee ID
     employee = frappe.db.get_value("Employee", {"user_id":doc.owner}, "name")
