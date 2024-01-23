85691d0fa73669673762e9e45e662653b151e1e3
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-01-23 17:11:09 +0545

    Update api.py

--- raindrop/api.py
+++ raindrop/api.py
@@ -1081,12 +1081,12 @@ def create_sales_invoice_2023():
                 print(f'{e} {value[2]}')
 
 def create_jounal_entry():
-    with open('/home/frappe/frappe-bench/apps/raindrop/Journal Voucher Number 2024 - Sheet1.csv') as design_file:
+    with open('/home/frappe/frappe-bench/apps/raindrop/Journal Voucher Number 2024 - Sheet1_f.csv') as design_file:
         reader_po = csv.reader(design_file, delimiter=',')
         for value in reader_po:
             try:
                 items = []
-                with open('/home/frappe/frappe-bench/apps/raindrop/Journal Voucher 2024 - Sheet_1.csv') as templates:
+                with open('/home/frappe/frappe-bench/apps/raindrop/Journal Voucher 2024 - Sheet1_s.csv') as templates:
                     reader = csv.reader(templates, delimiter=',')
                     items.clear()
                     for row in reader:
