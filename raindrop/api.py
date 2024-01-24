eb734c9f448c0c505a6382397773f2dc2f3acadf
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-01-24 12:38:29 +0545

    Update api.py

--- raindrop/api.py
+++ raindrop/api.py
@@ -660,7 +660,7 @@ def create_purchase_order_2023():
 
 
 def create_goods_received_2023():
-    with open('/home/frappe/frappe-bench/apps/raindrop/GRN HPL 2020 to 2023 - Sheet1 (1).csv' ) as design_file:
+    with open('/home/frappe/frappe-bench/apps/raindrop/Purchase received Number  - Sheet1.csv' ) as design_file:
         reader_po = csv.reader(design_file, delimiter=',')
         for value in  reader_po:
             try:
