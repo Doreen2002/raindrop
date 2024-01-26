66adec6d8a883f80051ce29917849d19d9bec9d6
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-01-26 15:24:27 +0200

    Update api.py

--- raindrop/api.py
+++ raindrop/api.py
@@ -766,14 +766,14 @@ def create_goods_received_2023():
 
 
 def create_purchase_invoice_2020():
-    with open( '/home/frappe/frappe-bench/apps/raindrop/HPL Purchase Invoice Number NPR 2020_2013 - Sheet1 (1).csv') as design_file:
+    with open( '/home/frappe/frappe-bench/apps/raindrop/Purchase_Invoice 2024 Number - Sheet1.csv') as design_file:
         reader_po = csv.reader(design_file, delimiter=',')
         for value in  reader_po:
             try:
                 items = []
                 taxes = []
                 tax_template = []
-                with open('/home/frappe/frappe-bench/apps/raindrop/HPL Purchase Invoice NPR 2020_2013 - Sheet1 (3).csv' ) as templates:
+                with open('/home/frappe/frappe-bench/apps/raindrop/Purchase_Invoice 2024 - Sheet1.csv' ) as templates:
                     reader = csv.reader(templates, delimiter=',')
                     for row in reader:
                         cost_center = "Main - HPL"
