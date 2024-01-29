112567d54ecd11659fe41482f25abf5b5674c1e3
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-01-29 05:42:38 +0200

    Update purchase_order.py

--- raindrop/custom_code/purchase_order.py
+++ raindrop/custom_code/purchase_order.py
@@ -20,7 +20,7 @@ def on_update(doc, method):
             total += item.amount
         limit_amount = frappe.db.get_value("Employee", {"user_id":frappe.session.logged_in_user}, "custom_purchase_approval_limit") 
         if "General Manager" not  in frappe.get_roles() :
-            if total > limit_amount:
+            if total > float(f"{limit_amount}"):
                     frappe.throw(f"The Material Purchase Is Above Limit, Send to General Manager or Immediate Manger. Limit is {limit_amount} and total amount on PO is {total} ")
                
     
