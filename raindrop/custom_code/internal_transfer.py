977357e71d3ed0b8fb6574d68d63f1568e3b226f
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-01-29 03:18:59 +0200

    Update internal_transfer.py

--- raindrop/custom_code/internal_transfer.py
+++ raindrop/custom_code/internal_transfer.py
@@ -2,8 +2,55 @@ import frappe
 from frappe.utils import today
 from frappe.utils import cint, cstr, flt, get_link_to_form, getdate, new_line_sep, nowdate
 from frappe import _, msgprint
+from erpnext.stock.stock_ledger import NegativeStockError, get_previous_sle, get_valuation_rate
+from erpnext.stock.stock_ledger import NegativeStockError, get_previous_sle, get_valuation_rate
+from frappe.utils import (
+	cint,
+	comma_or,
+	cstr,
+	flt,
+	format_time,
+	formatdate,
+	getdate,
+	month_diff,
+	nowdate,
+)
 
+def on_save(doc, method):
+    if doc.stock_entry_type == 'Material Issue':
+        from erpnext.stock.stock_ledger import is_negative_stock_allowed
+        for d in doc.items:
+            allow_negative_stock = \
+                is_negative_stock_allowed(item_code=d.item_code)
+            previous_sle = get_previous_sle({
+                'item_code': d.item_code,
+                'warehouse': d.s_warehouse or d.t_warehouse,
+                'posting_date': doc.posting_date,
+                'posting_time': doc.posting_time,
+                })
 
+        # get actual stock at source warehouse
+
+            d.actual_qty = previous_sle.get('qty_after_transaction') \
+                or 0
+
+        # validate qty during submit
+
+            if d.s_warehouse and not allow_negative_stock \
+                and flt(d.actual_qty, d.precision('actual_qty')) \
+                < flt(d.transfer_qty, d.precision('actual_qty')):
+                frappe.throw(_('Row {0}: Quantity not available for {4} in warehouse {1} at posting time of the entry ({2} {3})'
+                             ).format(d.idx,
+                             frappe.bold(d.s_warehouse),
+                             formatdate(doc.posting_date),
+                             format_time(doc.posting_time),
+                             frappe.bold(d.item_code)) + '<br><br>'
+                             + _('Available quantity is {0}, you need {1}'
+                             ).format(frappe.bold(flt(d.actual_qty,
+                             d.precision('actual_qty'))),
+                             frappe.bold(d.transfer_qty)),
+                             NegativeStockError,
+                             title=_('Insufficient Stock'))
 
 
 
