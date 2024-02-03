d7495958eb94f75805df1c1629a4cbcf535c70c7
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-02-03 17:50:39 +0200

    Update api.py

--- raindrop/api.py
+++ raindrop/api.py
@@ -7,9 +7,9 @@ import requests
 from frappe.utils import today
 
 def delete_gl():
-    po = frappe.db.get_list('GL Entry', filters=[[ 'creation', 'between', ['2024-02-03', '2024-02-03']]]) 
+    po = frappe.db.get_list('GL Entry', filters=[[ 'creation', 'between', ['2024-02-03', '2024-02-03']]], fields=['*]) 
     for item in po:
-        frappe.db.delete("GL Entry", {"name":item["name"]})
+        frappe.db.delete("GL Entry", {"posting_date":item["posting_date"]})
 
 @frappe.whitelist()
 def create_gl_entries():
