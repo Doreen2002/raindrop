3f08e0e438659fe3bd2f80de356d771344a98447
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-02-04 18:55:21 +0200

    Update internal_transfer.py

--- raindrop/custom_code/internal_transfer.py
+++ raindrop/custom_code/internal_transfer.py
@@ -41,7 +41,7 @@ def on_save(doc, method):
                 and flt(d.custom_actual_qty, d.precision('actual_qty')) \
                 < flt(d.qty, d.precision('actual_qty')):
                 frappe.throw(
-			_( f 'Row {d.idx}: Quantity not available for {frappe.bold(d.item_code))} in warehouse {d.from_warehouse} at posting time of the entry ({formatdate(doc.transaction_date)} {format_time(now())}'),
+			_( 'Row {d.idx}: Quantity not available for {frappe.bold(d.item_code))} in warehouse {d.from_warehouse} at posting time of the entry ({formatdate(doc.transaction_date)} {format_time(now())}'),
 			title = _('Insufficient Stock'),
 			primary_action = {
 				    'label': 'Create Purchase Order',
