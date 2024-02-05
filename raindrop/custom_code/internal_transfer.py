2fb753a728c4c9966ae0c9051419f93232cb701e
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-02-05 04:29:17 +0200

    Update internal_transfer.py

--- raindrop/custom_code/internal_transfer.py
+++ raindrop/custom_code/internal_transfer.py
@@ -44,7 +44,7 @@ def on_save(doc, method):
 				raise_exception= NegativeStockError,
 				primary_action={
 				        'label': _('Create Purchase Order'),
-				        'server_action': 'raindrop.custom_code.internal.server.create_purchase_order',
+				        'server_action': 'raindrop.custom_code.internal_transfer.create_purchase_order',
 				        # 'client_action': 'dotted.path.to.client.method',
 				        'hide_on_success': True,
 				        # 'args': json.dumps(doc)
