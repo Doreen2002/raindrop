8965c0496009f147a66ddfe27c5bfb9940aa4876
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-02-07 04:16:21 +0200

    Update purchase_order.py

--- raindrop/custom_code/purchase_order.py
+++ raindrop/custom_code/purchase_order.py
@@ -1,7 +1,8 @@
 import frappe 
 
 def before_insert(doc, method):
-    
+    if doc.custom_initiator != None:
+        doc.owner = doc.custom_initiator
     doc.custom_purchase_approver__id = add_approver(doc.modified_by, doc.cost_center)
     doc.custom_initiator_manager = add_approver(doc.owner, doc.cost_center)
     doc.custom_purchase_request_manager = add_approver(doc.owner, doc.cost_center)
