36b4efe8543de5378ddd8920b9a41055cdd4ea91
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-02-07 12:26:23 +0200

    Update internal_transfer.py

--- raindrop/custom_code/internal_transfer.py
+++ raindrop/custom_code/internal_transfer.py
@@ -38,7 +38,7 @@ def on_save(doc, method):
 
         # validate qty during submit
 			
-            if d.from_warehouse and not allow_negative_stock  and flt(d.custom_actual_qty, d.precision('actual_qty')) < flt(d.qty, d.precision('actual_qty')):
+            if d.qty > q.actual_qty:
                 frappe.msgprint(msg=f"Row {d.idx}: Quantity not available for {frappe.bold(d.item_code)} in warehouse {d.from_warehouse} at posting time of the entry {formatdate(doc.transaction_date)} {format_time(now())} ",
 				title='Insuffiecient Stock',
 				raise_exception= NegativeStockError
