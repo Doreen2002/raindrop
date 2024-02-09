3cb0e9c5ab38bd602b33fa6c82007d9acc3e93da
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-02-09 15:03:52 +0200

    Update stock_entry.js

--- raindrop/public/js/stock_entry.js
+++ raindrop/public/js/stock_entry.js
@@ -22,13 +22,13 @@ before_save(frm)
 		{ 
 			if (frm.doc.stock_entry_type == "Material Issue")
 			{
-			$('div:contains("Target Warehouse")').hide()
+			$('.static-area:contains("Target Warehouse")').hide()
 			$('div[data-fieldname="t_warehouse"]').hide()
 			$('div[data-fieldname="s_warehouse"]').show()
 			}
 			if (frm.doc.stock_entry_type == "Material Receipt")
 			{
-			$('div:contains("Source Warehouse")').hide()
+			$('.static-area ellipsis:contains("Source Warehouse")').hide()
 			$('div[data-fieldname="s_warehouse"]').hide()
 			$('div[data-fieldname="t_warehouse"]').show()
 			}
