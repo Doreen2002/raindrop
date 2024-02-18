b37196f2b73c2e6d7176786a09c9a869902c8da4
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-02-18 12:24:50 +0200

    Update purchase_order_list.js

--- raindrop/public/js/purchase_order_list.js
+++ raindrop/public/js/purchase_order_list.js
@@ -2,5 +2,11 @@ frappe.listview_settings['Purchase Order'] = {
 
     hide_name_column: true, // hide the last column which shows the `name`
     hide_name_filter: true, // hide the default filter field for the name column
+     get_indicator(doc) {
+        // customize indicator color
+        if (doc.docstatus == 2) {
+            return [__("Cancelled"), "red", "workflow_state,=,Cancelled"];
+        }
+    },
   
 }
