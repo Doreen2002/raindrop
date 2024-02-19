9651a9a86ee0b68ccfcb1eb80ee44f0f40319e83
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-02-19 04:49:36 +0200

    Update purchase_order.py

--- raindrop/custom_code/purchase_order.py
+++ raindrop/custom_code/purchase_order.py
@@ -32,6 +32,18 @@ def on_update(doc, method):
         if "General Manager" not  in frappe.get_roles() :
             if total > limit_amount :
                     frappe.throw(f"The Material Purchase Is Above Limit, Send to General Manager or Immediate Manger. Limit is {limit_amount } and total amount on PO is {total} ")
+        if frappe.db.get_value("Supplier", doc.supplier, "email") != None:
+            email_args = {
+				"recipients": frappe.db.get_value("Supplier", doc.supplier, "email"),
+				"message": _("Purchase Order Approved"),
+				"subject": 'Purchase Order Approved From  {0} '.format(doc.company),
+				"attachments": [frappe.attach_print(doc.doctype, doc.name, file_name=doc.name)],
+				"reference_doctype": doc.doctype,
+				"reference_name": doc.name
+				}
+			enqueue(method=frappe.sendmail, queue='short', timeout=300, async=True, **email_args)
+		else:
+			msgprint(_("{0}: Supplier email not found, hence email not sent").format(doc.supplier))
                
     
 
