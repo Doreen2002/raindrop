2bf187ef93bb221ba1714610ba3a0830e11b8c53
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-02-21 12:38:14 +0200

    Update api.py

--- raindrop/api.py
+++ raindrop/api.py
@@ -1514,13 +1514,13 @@ def create_payment():
 
 
 def stock_out():
-    with open('/home/frappe/frappe-bench/apps/raindrop/HPL Inventory Adjustment  Number 2024 - Sheet1.csv') as design_file:
+    with open('/home/frappe/frappe-bench/apps/raindrop/HPL Inventory Adjustment Number 2020_2023 - Goods Issues.csv') as design_file:
         reader_po = csv.reader(design_file, delimiter=',')
         for value in reader_po:
             try:
                 items = []
                 recieved = []
-                with open('/home/frappe/frappe-bench/apps/raindrop/HPL Inventory Adjustment 2024 - Sheet1.csv' ) as templates:
+                with open('/home/frappe/frappe-bench/apps/raindrop/HPL Inventory Adjustment 2020_2023 - Goods Issues.csv' ) as templates:
                     reader = csv.reader(templates, delimiter=',')
                     items.clear()
                     for row in reader:
