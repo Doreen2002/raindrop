c4e0b8cca5021c7f201f8e9ca14871b1547bc9da
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-02-23 23:26:25 +0545

    Update travel_request.js

--- raindrop/public/js/travel_request.js
+++ raindrop/public/js/travel_request.js
@@ -1,1 +1,33 @@
-
+frappe.ui.form.on("Travel Request", {
+  onload_post_render: function(frm){
+    frappe.call({
+            method: 'raindrop.custom_code.travel_request.get_approver',
+            args: {
+                owner: frm.doc.owner,
+		
+            },
+	            freeze: true,
+	            callback: (r) => {
+			console.log(r.message)
+	                if(r.message.length > 1)
+			{
+			frm.set_query('custom_cost_center', () => {
+	                return {
+	                    filters: {
+	                        name: ['in', r.message]
+	                    }
+	                }
+	            })
+			}
+			 if(r.message.length == 1)   
+			 {
+				frm.doc.custom_cost_center = r.message[0]
+				 frm.refresh_fields()
+			 }
+	            },
+	            error: (r) => {
+	                console.log(r)
+	            }
+        })
+  }
+}
