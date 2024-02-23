750125deb89f48a7c0312fc1408b4eda4d1ffe82
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-02-24 00:52:30 +0545

    Update travel_request.js

--- raindrop/public/js/travel_request.js
+++ raindrop/public/js/travel_request.js
@@ -1,15 +1,14 @@
 frappe.ui.form.on("Travel Request", {
 
   onload_post_render: function(frm){
-	  	if (frm.doc.workflow_state == "Approved" && frm.doc.owner == frappe.session.logged_in_user || frappe.user.has_role("Administrator"))
-			
-		 {
-			 $("button:contains('Create')").show();
-		}
-	if (frm.doc.workflow_state == "Pending" && frm.doc.custom_travel_request_approver != frappe.session.logged_in_user && !frappe.user.has_role("Administrator"))
-		 {
-			$('.actions-btn-group').hide()
-		}
+	  if (frm.doc.workflow_state == "Approved" && frm.doc.owner == frappe.session.logged_in_user || frappe.user.has_role("Administrator"))
+	  {
+		  $("button:contains('Create')").show()
+	  }
+	  if (frm.doc.workflow_state == "Pending" && frm.doc.custom_travel_request_approver != frappe.session.logged_in_user && !frappe.user.has_role("Administrator"))
+	  {
+		  $('.actions-btn-group').hide()
+	  }
     frappe.call({
             method: 'raindrop.custom_code.travel_request.get_approver',
             args: {
