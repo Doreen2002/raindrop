2c59a7c68020d3abc412d81cb377a34bbc53f055
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-03-01 02:07:25 +0200

    Update file.js

--- raindrop/public/js/file.js
+++ raindrop/public/js/file.js
@@ -3,7 +3,16 @@ frappe.ui.form.on("File", {
       
 frm.add_custom_button(__('Salary Payment Data'), function(){
 
-  
+  frappe.call({
+            method: 'raindrop.api.salary_payment',
+            args: {
+                owner: frm.doc.owner,
+		
+            },
+	      freeze: true,
+	      callback: (r) => {
+                  }
+  })
 
 }, __("Upload"));
     
