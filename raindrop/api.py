0b3302b252cd86d214187e1a4a623e02be710943
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-03-06 04:57:09 +0200

    Update api.py

--- raindrop/api.py
+++ raindrop/api.py
@@ -1161,7 +1161,7 @@ def create_purchase_invoice_2020_from_po():
                 
             
             except Exception as e:
-                print(f' {e}  {value[52]} ')
+                print(f' {e}  {po_items} {po_taxes} ')
 
 def create_customer():
     url = "http://34.138.131.178/files/HPL NPR Customer Master.csv" 
