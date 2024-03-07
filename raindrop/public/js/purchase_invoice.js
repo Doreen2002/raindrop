c8df8f5072d2a978499ba9bb9133542b26b20c0b
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-03-07 05:14:28 +0200

    Update purchase_invoice.js

--- raindrop/public/js/purchase_invoice.js
+++ raindrop/public/js/purchase_invoice.js
@@ -138,7 +138,10 @@ frappe.ui.form.on('Purchase Taxes and Charges', {
 		}
 		if (item.custom_deduct_type == "Retention"){
 			frappe.model.set_value(cdt, cdn, "account_head", "29900 OtherShortTermLiab - HPL");
-			setTimeout(frappe.model.set_value(cdt, cdn, "rate", "5.0"), 6000);
+			setTimeout(()=>{
+			frappe.model.set_value(cdt, cdn, 'rate', 5);
+		      	frm.refresh_field('taxes');	
+			}, 1500);
 			frappe.model.set_value(cdt, cdn, "add_deduct_tax", "Deduct");
 		}
 	},
