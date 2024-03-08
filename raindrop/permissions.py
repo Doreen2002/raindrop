1a8a12cd69d88cc59197fb856b1b1939e8024f14
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-03-08 12:11:53 +0200

    Update permissions.py

--- raindrop/permissions.py
+++ raindrop/permissions.py
@@ -1,4 +1,4 @@
-def todo_query():
+def purchase_order_query():
     if frappe.session.user != "Administrator"  or frappe.session.user != "umesh.sharma@hpl.com.np"  or frappe.session.user != "bimala.khadka@hpl.com.np" or frappe.session.user != "surya.karki@hpl.com.np":
         user = frappe.session.user
         return "(`tabPurchase Order`.owner = {user}  or `tabPurchase Order`.custom_purchase_approver__id = {user}  or `tabPurchase Order`.custom_initiator = {user}  or `tabPurchase Order`.custom_purchase_request_manager = {user} )".format(user=frappe.db.escape(user))
