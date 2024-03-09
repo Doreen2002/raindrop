3785b81aa1c34851bf107d3c334ee0ff652a1710
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-03-09 02:30:02 +0200

    Update permissions.py

--- raindrop/permissions.py
+++ raindrop/permissions.py
@@ -1,9 +1,10 @@
 import frappe
 
-def purchase_order_query(user, assigned=None):
+def purchase_order_query(user, assigned=[user]):
     if frappe.session.user != "Administrator"  or frappe.session.user != "umesh.sharma@hpl.com.np"  or frappe.session.user != "bimala.khadka@hpl.com.np" or frappe.session.user != "surya.karki@hpl.com.np":
         user = frappe.session.user
-        assigned = tuple(frappe.db.get_list("ToDo", pluck='allocated_to'))
+        if frappe.db.get_list("ToDo", pluck='allocated_to') != []:
+            assigned = tuple(frappe.db.get_list("ToDo", pluck='allocated_to'))
         return "(`tabPurchase Order`.owner = {user}  or `tabPurchase Order`.custom_purchase_approver__id = {user}  or `tabPurchase Order`.custom_initiator = {user}  or `tabPurchase Order`.custom_purchase_request_manager = {user} or {user} in {assigned} )".format(user=frappe.db.escape(user), assigned = assigned)
 
 # def purchase_invoice_query(user):
