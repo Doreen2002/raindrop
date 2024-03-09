772249770fbd0d4b6866891204d42b29fc20f65e
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-03-09 02:20:48 +0200

    Update permissions.py

--- raindrop/permissions.py
+++ raindrop/permissions.py
@@ -4,7 +4,7 @@ def purchase_order_query(user, assigned=None):
     if frappe.session.user != "Administrator"  or frappe.session.user != "umesh.sharma@hpl.com.np"  or frappe.session.user != "bimala.khadka@hpl.com.np" or frappe.session.user != "surya.karki@hpl.com.np":
         user = frappe.session.user
         assigned = frappe.db.get_list("ToDo", pluck='allocated_to')
-        return "(`tabPurchase Order`.owner = {user}  or `tabPurchase Order`.custom_purchase_approver__id = {user}  or `tabPurchase Order`.custom_initiator = {user}  or `tabPurchase Order`.custom_purchase_request_manager = {user} or {user} IN {assigned} )".format(user=frappe.db.escape(user), assigned =frappe.db.escape( assigned))
+        return "(`tabPurchase Order`.owner = {user}  or `tabPurchase Order`.custom_purchase_approver__id = {user}  or `tabPurchase Order`.custom_initiator = {user}  or `tabPurchase Order`.custom_purchase_request_manager = {user} or {user} in (`assigned`} )".format(user=frappe.db.escape(user))
 
 # def purchase_invoice_query(user):
 #     if frappe.session.user != "Administrator"  or frappe.session.user != "umesh.sharma@hpl.com.np"  or frappe.session.user != "bimala.khadka@hpl.com.np" or frappe.session.user != "surya.karki@hpl.com.np" or frappe.session.user != "keshav.kc@hpl.com.np":
