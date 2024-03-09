4df2b90f19ba89a49495e805b7d4f0c10c094fe4
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-03-09 16:11:05 +0545

    Update hooks.py

--- raindrop/hooks.py
+++ raindrop/hooks.py
@@ -12,7 +12,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/raindrop/css/raindrop.css"
+app_include_css = "/assets/raindrop/css/raindrop.css"
 # app_include_js = "/assets/raindrop/js/raindrop.js"
 
 # include js, css files in header of web template
