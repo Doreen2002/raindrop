a37c2e23c80e7e59f5fe115600e7017aa66203df
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-03-10 08:26:11 +0200

    Update permissions.py

--- raindrop/permissions.py
+++ raindrop/permissions.py
@@ -3,7 +3,8 @@ import frappe
 def purchase_order_query(user):
     if not user:
         user = frappe.session.user
-    if frappe.session.user != "Administrator"  or frappe.session.user != "umesh.sharma@hpl.com.np"  or frappe.session.user != "bimala.khadka@hpl.com.np" or frappe.session.user != "surya.karki@hpl.com.np" or  frappe.session.user != "keshav.kc@hpl.com.np":
+    user_list = ["Administrator", "umesh.sharma@hpl.com.np", "bimala.khadka@hpl.com.np", "surya.karki@hpl.com.np", "keshav.kc@hpl.com.np"]
+    if frappe.session.user not in user_list:
         return "(`tabPurchase Order`.owner = {user}  or `tabPurchase Order`.custom_purchase_approver__id = {user}  or `tabPurchase Order`.custom_initiator = {user}  or `tabPurchase Order`.custom_purchase_request_manager = {user}  )".format(user=frappe.db.escape(user))
         #return "(`tabPurchase Order`.owner =   or `tabPurchase Order`.custom_purchase_approver__id = {user}  or `tabPurchase Order`.custom_initiator = {user}  or `tabPurchase Order`.custom_purchase_request_manager = {user} or `tabPurchase Order`.assigned_to in {assigned} )".format(user=frappe.db.escape(user), assigned = assigned)
 
