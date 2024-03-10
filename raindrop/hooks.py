93052dcc1c25cb2a537c0065514518bcdaa14848
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-03-10 11:56:26 +0545

    Update hooks.py

--- raindrop/hooks.py
+++ raindrop/hooks.py
@@ -107,10 +107,10 @@ doctype_list_js = {"Purchase Order" : "public/js/purchase_order_list.js"}
 # -----------
 # Permissions evaluated in scripted ways
 
-# permission_query_conditions = {
-	# "Purchase Order": "raindrop.permissions.purchase_order_query",
+permission_query_conditions = {
+	"Purchase Order": "raindrop.permissions.purchase_order_query",
 	# "Purchase Invoice": "raindrop.permissions.purchase_invoice_query",
-# }
+}
 
 
 #
