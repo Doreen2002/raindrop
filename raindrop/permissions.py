b5a03d81bdabbc33516abd4558cad27e7db9437a
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-03-10 10:55:31 +0545

    Update permissions.py

--- raindrop/permissions.py
+++ raindrop/permissions.py
@@ -1,10 +1,10 @@
-import frappe
+# import frappe
 
-def purchase_order_query(user):
-    if not user:
-        user = frappe.session.user
-    if frappe.session.user != "Administrator"  or frappe.session.user != "umesh.sharma@hpl.com.np"  or frappe.session.user != "bimala.khadka@hpl.com.np" or frappe.session.user != "surya.karki@hpl.com.np":
-        return "(`tabPurchase Order`.owner = {user}  or `tabPurchase Order`.custom_purchase_approver__id = {user}  or `tabPurchase Order`.custom_initiator = {user}  or `tabPurchase Order`.custom_purchase_request_manager = {user}  )".format(user=frappe.db.escape(user))
+# def purchase_order_query(user):
+#     if not user:
+#         user = frappe.session.user
+#     if frappe.session.user != "Administrator"  or frappe.session.user != "umesh.sharma@hpl.com.np"  or frappe.session.user != "bimala.khadka@hpl.com.np" or frappe.session.user != "surya.karki@hpl.com.np":
+#         return "(`tabPurchase Order`.owner = {user}  or `tabPurchase Order`.custom_purchase_approver__id = {user}  or `tabPurchase Order`.custom_initiator = {user}  or `tabPurchase Order`.custom_purchase_request_manager = {user}  )".format(user=frappe.db.escape(user))
         #return "(`tabPurchase Order`.owner =   or `tabPurchase Order`.custom_purchase_approver__id = {user}  or `tabPurchase Order`.custom_initiator = {user}  or `tabPurchase Order`.custom_purchase_request_manager = {user} or `tabPurchase Order`.assigned_to in {assigned} )".format(user=frappe.db.escape(user), assigned = assigned)
 
 # def purchase_invoice_query(user):
