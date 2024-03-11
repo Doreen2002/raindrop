eaeb63fa26c969229e863be92cf04c4b786ac193
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-03-11 15:16:05 +0545

    Update permissions.py

--- raindrop/permissions.py
+++ raindrop/permissions.py
@@ -3,7 +3,7 @@ import frappe
 def purchase_order_query(user):
     if not user:
         user = frappe.session.user
-    user_list = ["Administrator", "umesh.sharma@hpl.com.np", "bimala.khadka@hpl.com.np", "surya.karki@hpl.com.np", "keshav.kc@hpl.com.np"]
+    user_list = ["Administrator", "umesh.sharma@hpl.com.np", "bimala.khadka@hpl.com.np", "surya.karki@hpl.com.np", "keshav.kc@hpl.com.np", "krishna.pradhan@hpl.com.np"]
     if frappe.session.user not in user_list:
         return "(`tabPurchase Order`.owner = {user}  or `tabPurchase Order`.custom_purchase_approver__id = {user}  or `tabPurchase Order`.custom_initiator = {user}  or `tabPurchase Order`.custom_purchase_request_manager = {user}  )".format(user=frappe.db.escape(user))
         #return "(`tabPurchase Order`.owner =   or `tabPurchase Order`.custom_purchase_approver__id = {user}  or `tabPurchase Order`.custom_initiator = {user}  or `tabPurchase Order`.custom_purchase_request_manager = {user} or `tabPurchase Order`.assigned_to in {assigned} )".format(user=frappe.db.escape(user), assigned = assigned)
