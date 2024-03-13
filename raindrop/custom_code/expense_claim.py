02c20a83f7482b422adfc29451289fef9c05da01
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-03-13 03:17:40 +0200

    Update expense_claim.py

--- raindrop/custom_code/expense_claim.py
+++ raindrop/custom_code/expense_claim.py
@@ -1,6 +1,11 @@
 import frappe 
 from frappe.utils import today
 
+def on_save(doc, method):
+    if doc.custom_travel_request != None:
+        frappe.db.set_value('Travel Request',doc.custom_travel_request , 'custom_expense_claim_link', doc.name)
+
+
 def on_update(doc, method):
     purchase_approver = frappe.db.get_value("Employee", {"user_id":doc.owner}, "custom_purchase_approver_id")
     if purchase_approver == '' or purchase_approver == None and "Administrator" not in frappe.get_roles():
