22661c6409fc70cc66fd079ed9266127bdcba82a
Doreen2002 <doreenmwapekatebe8@gmail.com>
2024-03-20 02:24:01 +0200

    modified api on purchase order function to pick correct cost center column

--- raindrop/api.py
+++ raindrop/api.py
@@ -762,7 +762,7 @@ def create_purchase_order_2023():
                     if value[6] == '' or value[6] == None:
                             doc.set_warehouse="KIRNE - HPL"
                     doc.custom_billable = value[48]
-                    doc.cost_center = f'{value[36]} - HPL'
+                    doc.cost_center = f'{value[12]} - HPL'
                     doc.custom_billing_address = value[26]
                     doc.custom_shipping_address = value [27]
                     doc.custom_expense_category = value[33]
