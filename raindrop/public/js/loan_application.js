7b78ae1ffd55ca84b3f49fab58e361f511ea6c3f
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2024-03-24 10:23:01 +0545

    Update loan_application.js

--- raindrop/public/js/loan_application.js
+++ raindrop/public/js/loan_application.js
@@ -1,1 +1,46 @@
+frappe.ui.form.on("Expense Claim", {
+onload_post_render: function(frm){
+	
+// if (frm.doc.workflow_state == "Pending" && frm.doc.expense_approver != frappe.session.logged_in_user && !frappe.user.has_role("Administrator"))
+// 	 {
+// 		$('.actions-btn-group').hide()
+// 	}
+// 	if ( frm.doc.workflow_state == "Draft" )
+// 	{
+// 		$('.actions-btn-group').show()
+// 	}
+  
+	cur_frm.set_df_property('approval_status', 'hidden', 1);
+  cur_frm.set_df_property('expense_approver', 'hidden', 1);
+  cur_frm.refresh_fields();
+	
 
+},
+
+    before_save(frm)
+        {
+            frappe.call({
+            method: 'raindrop.custom_code.expense_claim.add_approver',
+            args: {
+                owner: frm.doc.owner
+            },
+            freeze: true,
+            callback: (r) => {
+                frm.doc.expense_approver = r.message;
+                frm.refresh_fields();
+            },
+            error: (r) => {
+                console.log(r)
+            }
+            
+        })
+        },
+
+    // refresh(frm)
+    // {
+       
+    // },
+      
+
+    
+})
